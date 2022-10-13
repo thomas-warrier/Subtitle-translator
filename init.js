5d8a17b7c148085035c7b5117419e5693c069ac5
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-10-13 19:12:29 +0200

    ajout du get subtitles

--- init.js
+++ init.js
@@ -1,21 +1,35 @@
-(function AddButtons (){
-        var btn = document.createElement
-         ("input");
-         btn.value = "Translate Subtitles";
-         btn.id = "translate-btn";
-         btn.type = "submit";
-        document.querySelector("ltr-1jnlk6v").prepend(btn); //the query selector select the div where all of the right bottom button are
+(function (){
+    function AddButtons() {
+        var btn = document.createElement("input");
+        btn.value = "Translate Subtitles";
+        btn.id = "translate-btn";
+        btn.type = "submit";
+        document.querySelector("div.ltr-1jnlk6v").prepend(btn); //the query selector select the div where all of the right bottom button are
         //the prepend add the button before the childs example: parent.prepend(newChild)  will be [newChild, child1, child2]
     }
-    function DefineTranslateButtonEvents() {
-        document
-          .getElementById("translate-btn")
-          addEventListener("click", function
-          (event) {
-            fnSearch (event.target.value.split(" ")[1]);
-          });
-      }
+    function defineTranslateButtonEvents() {
+        document.getElementById("translate-btn")
+        addEventListener("click", function
+            (event) {
+            createPopUp();
+        });
+    }
+    
+    function createPopUp() {
+    
+    }
+    
+    function getSubtitles() {
+        let getChildren = document.querySelector("div.player-timedtext-text-container > span").children
+        var subtitlesTab = [];
+        for (let i = 0; i < getChildren.length; i++) {
+            subtitlesTab.push(getChildren[i].textContent)
+        }
+        return subtitlesTab
+    };
 
+    AddButtons();
+    defineTranslateButtonEvents();
+})();
 
-)();
 
