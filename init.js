d4868afaf940e19edb0a5fdd1a5d01d8a9d24fcb
Thomas <thomas.warrier@univ.etu-lyon1.fr>
2022-10-19 23:56:50 +0200

    avancement fonctionnement API

--- init.js
+++ init.js
@@ -1,111 +1,109 @@
-import tippy from 'tippy.js';
-import 'tippy.js/dist/tippy.css';
 (
     function () {
-    function addButtons() {
-        var btn = document.createElement("input");
-        btn.value = "";
-        btn.id = "translate-btn";
-        btn.type = "submit";
-        btn.onmouseout = function (event) {
-            const elements = document.getElementsByClassName("container-translation-ext");
-            while (elements.length > 0) {
-                elements[0].parentNode.removeChild(elements[0]);
-            }
-        };
-        btn.onmouseover = function (event) {
-            createPopUp()
-        };
+        function addButtons() {
+            var btn = document.createElement("input");
+            btn.value = "";
+            btn.id = "translate-btn";
+            btn.type = "submit";
+            btn.onmouseout = function (event) {
+                const elements = document.getElementsByClassName("container-translation-ext");
+                while (elements.length > 0) {
+                    elements[0].parentNode.removeChild(elements[0]);
+                }
+            };
+            btn.onmouseover = function (event) {
+                createPopUp()
+            };
 
-        waitForElm(".ltr-1jnlk6v").then((elm) => {
-            document.querySelectorAll(".ltr-1jnlk6v")[6].prepend(btn); //the query selector select the div where all of the right bottom button are
-            //the prepend add the button before the childs example: parent.prepend(newChild)  will be [newChild, child1, child2]
-        });
-    }
+            waitForElm(".ltr-1jnlk6v").then((elm) => {
+                document.querySelectorAll(".ltr-1jnlk6v")[6].prepend(btn); //the query selector select the div where all of the right bottom button are
+                //the prepend add the button before the childs example: parent.prepend(newChild)  will be [newChild, child1, child2]
+            });
+        }
 
-    function defineTranslateButtonEvents() {
-          
-    }
+        function defineTranslateButtonEvents() {
 
-   
+        }
 
-    function createPopUp() {
-        var popUp = document.createElement("div");
-        popUp.id = "PopUpTranslate"
-        popUp.innerHTML ="<div class='container-translation-ext'><div id='languages-container'><div id='from-languages'>Langue détectée</div><div id='to-languages'>Francais</div></div><div id='from-subtitles'><span>" + getSubtitles() + "</span></div><div id='translated-subtitles'><span>" + translateText(getSubtitles(),null,"FR") + "</span></div></div>"
-        
-        console.log(popUp.innerHTML)
-        console.log("PopUp created")
-    }
 
-    function translateText(subtitles,fromLanguage,toLanguage) {
-        let apiUrl = `https://api.mymemory.translated.net/get?q=${subtitles}&langpair=${fromLanguage}|${toLanguage}&de=twarrier69@gmail.com`;
-        // fetching api response and returning it with parsing into js obj
-        // and in another then method receiving that obj
-        fetch(apiUrl).then(res => res.json()).then(data =>{
-            console. log (data);
-        });
 
-    }
+        function createPopUp() {
+            var popUp = document.createElement("div");
+            popUp.id = "PopUpTranslate"
+            popUp.innerHTML = "<div class='container-translation-ext'><div id='languages-container'><div id='from-languages'>Langue détectée</div><div id='to-languages'>Francais</div></div><div id='from-subtitles'><span>" + getSubtitles() + "</span></div><div id='translated-subtitles'><span>" + translateText(getSubtitles(), "EN", "FR") + "</span></div></div>"
 
-    function getSubtitles() {
-        var childrenExist = document.querySelector("div.player-timedtext-text-container > span");
-        if (childrenExist !== null) {
-            let getChildren = document.querySelector("div.player-timedtext-text-container > span").children
+            console.log(popUp.innerHTML)
+            console.log("PopUp created")
+        }
+
+        function translateText(subtitles, fromLanguage, toLanguage) {
+            let apiUrl = `https://api.mymemory.translated.net/get?q=${subtitles}&langpair=${fromLanguage}|${toLanguage}&de=twarrier69@gmail.com`;
+            // fetching api response and returning it with parsing into js obj
+            // and in another then method receiving that obj
+            
+            fetch(apiUrl).then(res => res.json()).then(data => {
+                console.log(data);
+                var translatedText = data.responseData.translatedText;
+            });
+            return translatedText
+        }
+
+        function getSubtitles() {
+            var childrenExist = document.querySelector("div.player-timedtext-text-container > span");
+            if (childrenExist !== null) {
+                let getChildren = document.querySelector("div.player-timedtext-text-container > span").children
                 var subtitlesTab = [];
                 for (let i = 0; i < getChildren.length; i++) {
                     subtitlesTab.push(getChildren[i].textContent)
                 }
                 let stringSubtitles = subtitlesTab.join('\n')
                 return stringSubtitles;
-        } else {
-            console.log("there is no subtitle available")
+            } else {
+                console.log("there is no subtitle available")
+            }
         }
-    }
-
-    x
 
 
-    function waitForElm(selector) {
-        return new Promise(resolve => {
-            if (document.querySelector(selector)) {
-                return resolve(document.querySelector(selector));
+        function waitForElm(selector) {
+            return new Promise(resolve => {
+                if (document.querySelector(selector)) {
+                    return resolve(document.querySelector(selector));
+                }
+
+                const observer = new MutationObserver(mutations => {
+                    if (document.querySelector(selector)) {
+                        resolve(document.querySelector(selector));
+                        observer.disconnect();
+                    }
+                });
+
+                observer.observe(document.body, {
+                    childList: true,
+                    subtree: true
+                });
+            });
+        }
+        waitForElm(".ltr-omkt8s").then((elm) => {
+            console.log('Player is ready');
+            addButtons()
+            var selector = document.querySelector(".ltr-omkt8s")
+            function callback(mutationsList, observer) {
+                mutationsList.forEach(mutation => {
+                    if (mutation.attributeName === 'class') {
+                        if (selector.classList.contains("active")) {
+                            addButtons()
+                        }
+                    }
+                })
             }
 
-            const observer = new MutationObserver(mutations => {
-                if (document.querySelector(selector)) {
-                    resolve(document.querySelector(selector));
-                    observer.disconnect();
-                }
-            });
+            const mutationObserver = new MutationObserver(callback)
 
-            observer.observe(document.body, {
-                childList: true,
-                subtree: true
-            });
+            mutationObserver.observe(selector, { attributes: true })
         });
-    }
-    waitForElm(".ltr-omkt8s").then((elm) => {
-        console.log('Player is ready');
-        addButtons()
-        var selector = document.querySelector(".ltr-omkt8s")
-        function callback(mutationsList, observer) {
-            mutationsList.forEach(mutation => {
-                if (mutation.attributeName === 'class') {
-                    if (selector.classList.contains("active")) {
-                        addButtons()
-                    }
-                }
-            })
-        }
-
-        const mutationObserver = new MutationObserver(callback)
-
-        mutationObserver.observe(selector, { attributes: true })
-    });
 
 
-    defineTranslateButtonEvents();
-})();
+        defineTranslateButtonEvents();
+    })();
 
 
